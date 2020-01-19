03b3fea6901d1c9bdd8ed40235ecd9eb424547c1
RoshanShrestha123 <roshan.shrestha4u@gmail.com>
2020-01-19 20:02:20 +0545

    connecting to changed to API

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const API_ROUTE = require('./routes/api.routes');
+
+app.use(cors());
 
 app.use(express.urlencoded({
     extended:true //-> allows to send nested object
@@ -15,6 +18,6 @@ app.use((err,rep,res,next) => {
 app.use('/api',API_ROUTE);
 
 
-app.listen(3000,(err,res)=>{
+app.listen(4000,(err,res)=>{
     console.log("server started");
 })
